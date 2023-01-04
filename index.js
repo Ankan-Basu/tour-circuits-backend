9a02b300cb9eeaba72ddec89f876fa0dee7928d0
Ankan-Basu <60862627+Ankan-Basu@users.noreply.github.com>
2023-01-04 23:46:18 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -7,12 +7,17 @@ const {connectToDB, client} = require('./db');
 const fs = require('fs');
 const {convertCase} = require('./convertCase');
 
+const path = require('path');
+
+
 dotenv.config();
 const app = express();
 
 /*app.use(cors({
     origin: 'http://localhost:3000'
 }));*/
+
+app.use(express.static(path.join(__dirname, 'build')));
 
 const port = 5000
 
@@ -24,48 +29,51 @@ const db = 'tours_database';
 
 
 app.get('/', (req, res) => {
-  const id = req.query.id;
-
-  console.log(id);
-
-  // var dataToSend;
-
- // spawn new child process to call the python script
-//  const python = spawn('python', ['scrap2.py', id]);
-//  // collect data from script
-//  python.stdout.on('data', function (data) {
-//   console.log('Pipe data from python script ...');
-//   dataToSend = data.toString();
-//  });
-//  // in close event we are sure that stream from child process is closed
-//  python.on('close', (code) => {
-//  console.log(`child process close all stdio with code ${code}`);
-//  // send data to browser
-//  res.send(dataToSend)
-//  });
-
-
- // Call the python process and pass the
-// data as command line argument.
-  const py = spawn('python', ['scrapTopSights.py', id]);
-    
-  resultString = '';
-    
-  // As the stdout data stream is chunked,
-  // we need to concat all the chunks.
-  py.stdout.on('data', function (stdData) {
-    resultString += stdData.toString();
-  });
-    
-  py.stdout.on('end', function () {
-    
-    // Parse the string as JSON when stdout
-    // data stream ends
-    let resultData = JSON.parse(resultString);
-    // console.log(resultData);
-    res.send(resultData);
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 })
-})
+// app.get('/', (req, res) => {
+//   const id = req.query.id;
+
+//   console.log(id);
+
+//   // var dataToSend;
+
+//  // spawn new child process to call the python script
+// //  const python = spawn('python', ['scrap2.py', id]);
+// //  // collect data from script
+// //  python.stdout.on('data', function (data) {
+// //   console.log('Pipe data from python script ...');
+// //   dataToSend = data.toString();
+// //  });
+// //  // in close event we are sure that stream from child process is closed
+// //  python.on('close', (code) => {
+// //  console.log(`child process close all stdio with code ${code}`);
+// //  // send data to browser
+// //  res.send(dataToSend)
+// //  });
+
+
+//  // Call the python process and pass the
+// // data as command line argument.
+//   const py = spawn('python', ['scrapTopSights.py', id]);
+    
+//   resultString = '';
+    
+//   // As the stdout data stream is chunked,
+//   // we need to concat all the chunks.
+//   py.stdout.on('data', function (stdData) {
+//     resultString += stdData.toString();
+//   });
+    
+//   py.stdout.on('end', function () {
+    
+//     // Parse the string as JSON when stdout
+//     // data stream ends
+//     let resultData = JSON.parse(resultString);
+//     // console.log(resultData);
+//     res.send(resultData);
+// })
+// })
 
 function getData(id) {
   // Call the python process and pass the
