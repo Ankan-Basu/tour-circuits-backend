b47885c6361f4a8b1ab51ac9995c31de6ad510f2
Ankan-Basu <60862627+Ankan-Basu@users.noreply.github.com>
2023-01-05 00:23:03 +0530

    Add files via upload

--- db.js
+++ db.js
@@ -1,6 +1,8 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
-const dbURL = 'mongodb://localhost:27017'
+// const dbURL = "mongodb+srv://devbros:2DevBros%40HITK@cluster0.5q9v57a.mongodb.net/test"
+// const dbURL = 'mongodb://localhost:27017'
+const dbURL = 'mongodb+srv://devbros:2DevBros%40HITK@cluster0.5q9v57a.mongodb.net/?retryWrites=true&w=majority'
 
 const client = new MongoClient(dbURL, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
